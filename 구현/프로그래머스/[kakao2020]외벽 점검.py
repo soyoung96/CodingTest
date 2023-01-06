9a1982e6ebbf3b95654a2b320074988e62e76f4e
soyoung96 <psy411301@naver.com>
2023-01-07 04:18:15 +0900

    외벽점검 풀이 주석추가

--- 구현/프로그래머스/[kakao2020]외벽 점검.py
+++ 구현/프로그래머스/[kakao2020]외벽 점검.py
@@ -9,10 +9,10 @@ def delWeakE(weakcopy,distE,startWeakE,n): #15 #커버한 weak 삭제
             
     else: #n-1에서 0 이후로 넘어가지 않음
         delWeakList = []
-        for weakE in weakcopy:
+        for weakE in weakcopy: #for문에 사용되는 리스트는 pop이나 append 같은거로 변경하는 거 아님!!! -> 오류조심!
             if(startWeakE<= weakE <= endDist):
                 delWeakList.append(weakE)
-        for delWeakE in delWeakList:
+        for delWeakE in delWeakList: 
             weakcopy.pop(weakcopy.index(delWeakE))
         
 def solution(n, weak, dist):
