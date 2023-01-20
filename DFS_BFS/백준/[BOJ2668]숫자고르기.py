2c884903eab45957f2e5b2b8a522e43aab523d02
soyoung96 <psy411301@naver.com>
2023-01-20 11:06:39 +0900

    숫자고르기 풀이 개선

--- DFS_BFS/백준/[BOJ2668]숫자고르기.py
+++ DFS_BFS/백준/[BOJ2668]숫자고르기.py
@@ -1,16 +1,25 @@
 import sys
 sys.setrecursionlimit(int(1e7))
 
+# def dfs(startInd,ind,chart,dfsIterNum,visited): #O(100)
+#     visited[ind] = True #방문    
+#     if(chart[ind] == startInd):
+#         ans[0] = dfsIterNum
+#     else:
+#         if(visited[chart[ind]] != True): #방문한 적 없으면
+#             dfs(startInd,chart[ind],chart,dfsIterNum+1,visited)
+#         else:
+#             ans[0] = -1 #같은 집합 불가
+
 def dfs(startInd,ind,chart,dfsIterNum,visited): #O(100)
     visited[ind] = True #방문    
     if(chart[ind] == startInd):
-        ans[0] = dfsIterNum
+        return dfsIterNum
     else:
         if(visited[chart[ind]] != True): #방문한 적 없으면
-            dfs(startInd,chart[ind],chart,dfsIterNum+1,visited)
+            return dfs(startInd,chart[ind],chart,dfsIterNum+1,visited)
         else:
-            ans[0] = -1 #같은 집합 불가
-
+            return -1 #같은 집합 불가
 
 n = int(sys.stdin.readline())
 
@@ -20,15 +29,26 @@ for _ in range(n):
     chart.append(int(sys.stdin.readline()))
 
 maxAns =0
-ans = [0]
+# ans = [0]
+
+# maxVisited = []
+# for startInd in range(1,n+1): #O(100*300)
+#     if(startInd not in maxVisited): #이미 집합 포함된 곳은 방문 X    
+#         visited = [False] *(n+1) #O(100)
+#         dfs(startInd,startInd,chart,1,visited)#O(100)
+#         if(ans[0]>0): #같은 집합 결과 나옴
+#             maxAns += ans[0] # maxAns갱신
+#             for ind in range(1,n+1):#O(100)
+#                 if(visited[ind] == True):
+#                     maxVisited.append(ind)
 
 maxVisited = []
 for startInd in range(1,n+1): #O(100*300)
     if(startInd not in maxVisited): #이미 집합 포함된 곳은 방문 X    
         visited = [False] *(n+1) #O(100)
-        dfs(startInd,startInd,chart,1,visited)#O(100)
-        if(ans[0]>0): #같은 집합 결과 나옴
-            maxAns += ans[0] # maxAns갱신
+        ans = dfs(startInd,startInd,chart,1,visited)#O(100)
+        if(ans>0): #같은 집합 결과 나옴
+            maxAns += ans # maxAns갱신
             for ind in range(1,n+1):#O(100)
                 if(visited[ind] == True):
                     maxVisited.append(ind)
